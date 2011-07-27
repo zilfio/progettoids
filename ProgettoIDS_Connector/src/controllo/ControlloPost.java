69e7d38d39e0f8db06890c5e6036551f55677adc
zilfio88 <zilfio88@2f80cc07-2a41-b401-2d1e-b8ed4b7fd3b7>
2011-07-27 09:59:13 +0000

    controlli ed eccezioni

--- ProgettoIDS_Connector/src/controllo/ControlloPost.java
+++ ProgettoIDS_Connector/src/controllo/ControlloPost.java
@@ -6,38 +6,45 @@ import java.util.Collection;
 import rss.FeedMessage;
 
 public class ControlloPost {
-	public Collection<FeedMessage> checkpost (Collection<FeedMessage>postA, Collection<FeedMessage>postB){
-
+	public static Collection<FeedMessage> checkpost (Collection<FeedMessage>postA, Collection<FeedMessage>postB){
+		
 		Collection<FeedMessage> c = new ArrayList<FeedMessage>();
 		boolean trovato;
 		
 		//Controllo da A a B
-		for(FeedMessage f : postA){
-			trovato = false;
-			for(FeedMessage m : postB){
-				if((f.getTitle().equals(m.getTitle()))){
-					trovato = true;
+		if(postA!=null){
+			for(FeedMessage f : postA){
+				trovato = false;
+				if(postB!=null){
+					for(FeedMessage m : postB){
+						if((f.getTitle().equals(m.getTitle()))){
+							trovato = true;
+						}
+					}
+				}
+				if(!trovato){
+					c.add(f);
 				}
 			}
-			if(!trovato){
-				c.add(f);
-			}
 		}
+		
 		
 		//Controllo da B ad A
-		for(FeedMessage f : postB){
-			trovato = false;
-			for(FeedMessage m : postA){
-				if((f.getTitle().equals(m.getTitle()))){
-					trovato = true;
+		if(postB!=null){
+			for(FeedMessage f : postB){
+				trovato = false;
+				if(postA!=null){
+					for(FeedMessage m : postA){
+						if((f.getTitle().equals(m.getTitle()))){
+							trovato = true;
+						}
+					}
+				}
+				if(!trovato){
+					c.add(f);
 				}
 			}
-			if(!trovato){
-				c.add(f);
-			}
 		}
-		
-		
 		return c;
 	}
 }
--- ProgettoIDS_Connector/src/lettura/LetturaFeedback.java
+++ ProgettoIDS_Connector/src/lettura/LetturaFeedback.java
@@ -3,6 +3,8 @@ package lettura;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import javax.xml.stream.XMLStreamException;
+
 import rss.FeedMessage;
 
 import manager.Feedback;
@@ -12,16 +14,21 @@ import manager.Post;
 
 public class LetturaFeedback {
 	public static FeedMessage parsingFeed(String guid){
-		
 		Collection<FeedMessage> feedToT = LetturaPost.parsingPost(guid);
-		String media = MediaFeedback.calcola_media(feedToT);
+		try{
+			String media = MediaFeedback.calcola_media(feedToT);
+			
+			// creazione feedback univoco
+			FeedMessage feed = new FeedMessage();
+			feed.setDescription("Media Matematica Personalizzata");
+			feed.setAuthor("Connector");
+			feed.setTitle(media);
+			
+			return feed;
+		}
+		catch(NullPointerException e){
+			return null;
+		}
 		
-		// creazione feedback univoco
-		FeedMessage feed = new FeedMessage();
-		feed.setDescription("Media Matematica Personalizzata");
-		feed.setAuthor("Connector");
-		feed.setTitle(media);
-		
-		return feed;
 	}
 }
